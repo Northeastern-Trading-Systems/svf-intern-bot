0c13909bf2a7ea187d68565cd67640efad512031
milesChild <colechild1@gmail.com>
2022-10-03 21:31:44 -0400

    deleted and renewed app - now works as an intern @ CS

--- bot.py
+++ bot.py
@@ -11,20 +11,21 @@ env_path = Path('.') / 'env'  # denotes where path for the file is so we can loa
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-# signing secret, route for where events are sent, webserver we send to
-slack_event_adapter = SlackEventAdapter('3cc5f9d50eeb60a27396f344565d1b9f', '/slack/events', app)
 
-# token concealed from github
-# client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client = slack.WebClient(token='xoxb-4152885456631-4167497398114-ySKWpbX3YfH4dvCvg48Jn8aG')
-BOT_ID = client.api_call('auth.test')['user_id']  # obtains the id of the bot
+slack_event_adapter = SlackEventAdapter('8f57d4585beb8feaf5f5876d536930a9', '/slack/events', app)
+
+client = slack.WebClient(token='xoxb-4152885456631-4169299575394-qgBStFyvafuhOOUcqDp2gRqB')
+client.chat_postMessage(channel='#svf-slack-bot', text='Testing again')
 
 """
 Event method:
-
 When an event occurs in a channel, this method is triggered.
 Method echoes the string message that triggered the event.
 """
+# TODO: Subscribe to all events in a #intern channel and listen for messages that start with '!intern'
+
+BOT_ID = client.api_call('auth.test')['user_id']  # obtains the id of the bot
+
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})  # look for 'event' in payload, if it is not found then return an empty dict
@@ -37,7 +38,6 @@ def message(payload):
 
 """
 Command method:
-
 When user calls /get-random in slack, the bot will return a message:
 @<user>, your random number is <random num>.
 """
@@ -50,5 +50,12 @@ def get_random():
 
     client.chat_postMessage(channel=channel_id, text=f'@{user_id}, your random number is {rand}.')
 
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)  # default port 5000, debug = if we modify, flask will auto-modify the deployed file
+    app.run(debug=True)  # default port 5000, debug = if we modify, flask will auto-modify the deployed file
