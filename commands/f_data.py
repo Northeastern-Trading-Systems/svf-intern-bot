38b05ba907010163ccfc8a568652fda9dd92e6d9
milesChild <colechild1@gmail.com>
2022-10-05 21:40:32 -0400

    p

---
+++ commands/f_data.py
@@ -0,0 +1,17 @@
+from openbb_terminal.api import openbb
+"""
+Examples:
+insiders AAPL
+"""
+class Fundamental_Data:
+    ticker: str
+
+    def __init__(self, ticker):
+        self.ticker = ticker
+
+    def execute(self):
+        try:
+            result = str(openbb.stocks.fa.data(self.ticker))
+            return result
+        except IndexError as e:
+            raise ValueError("Please provide a symbol for fundamental data, e.g. <!intern fd AAPL>")
