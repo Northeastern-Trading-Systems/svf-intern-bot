44e482d9ce97c7ca0f38f76143747b2485b44cff
rothcharlie1 <43792220+rothcharlie1@users.noreply.github.com>
2022-10-07 19:39:42 -0400

    use fmp for income

--- commands/income.py
+++ commands/income.py
@@ -3,6 +3,8 @@ from openbb_terminal.api import openbb
 Examples:
 income AAPL
 """
+
+
 class Income_Stmt:
     ticker: str
 
@@ -11,7 +13,8 @@ class Income_Stmt:
 
     def execute(self):
         try:
-            result = str(openbb.stocks.fa.av_income(self.ticker))
+            result = str(openbb.stocks.fa.fmp_income(self.ticker))
             return result
         except IndexError as e:
-            raise ValueError("Please provide a symbol for income statement, e.g. <!intern pt AAPL>")
+            raise ValueError(
+                "Please provide a symbol for income statement, e.g. <!intern pt AAPL>")
