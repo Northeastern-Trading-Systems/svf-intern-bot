50c9647efb2853e4dd4bf802ab6650b9157631f5
rothcharlie1 <43792220+rothcharlie1@users.noreply.github.com>
2022-10-07 16:35:41 -0400

    Initial janky DCF situation

--- commands/dcf.py
+++ commands/dcf.py
@@ -1,14 +1,16 @@
 from openbb_terminal.api import openbb
+from openbb_terminal.stocks.fundamental_analysis import dcf_view
+
 
 class DCF:
-    ticker: str
 
     def __init__(self, ticker):
         self.ticker = ticker
 
     def execute(self):
         try:
-            result = "Command not yet supported..."
-            return result
+            workbook = dcf_view.CreateExcelFA(self.ticker).create_workbook()
+            return workbook
         except IndexError as e:
-            raise ValueError("Please provide a symbol for price target history, e.g. <!intern pt AAPL>")
+            raise ValueError(
+                "Please provide a symbol for price target history, e.g. <!intern pt AAPL>")
