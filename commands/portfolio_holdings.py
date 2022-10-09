b5feac7c8fbd9a98dc661eab8a9ba75ecaa27e5e
milesChild <colechild1@gmail.com>
2022-10-09 19:44:38 -0400

    updated port info

--- commands/portfolio_holdings.py
+++ commands/portfolio_holdings.py
@@ -17,7 +17,7 @@ class Portfolio_Holdings:
         weights = ['8.50%', '6.24%', '8.40%', '7.08%', '6.10%', '4.38%',
                    '5.56%', '5.04%', '3.72%', '4.12%', '6.71%', '34.14%']
         cost_basis = ['0.00', '0.00', '0.00', '0.00', '0.00',
-                      '0.00', '0.00', '0.00', '0.00', '0.00', '0.00', '0.00']
+                      '0.00', '0.00', '0.00', '0.00', '0.00', '3.01', '0.00']
         portfolio_dict = {'weight': weights, 'cost basis': cost_basis}
         self.portfolio_df = pd.DataFrame(portfolio_dict, index=tickers)
 
